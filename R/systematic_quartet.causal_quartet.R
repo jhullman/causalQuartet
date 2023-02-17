0905ef14f9fc8f7e96ae9bfffe942ad3603835a4
Jessica Hullman <jessicahullman@Jessicas-MacBook-Pro-56.local>
2023-02-17 12:48:30 -0600

    add support for input yrange to systematic quartet and random observables quartet

--- R/systematic_quartet.causal_quartet.R
+++ R/systematic_quartet.causal_quartet.R
@@ -13,6 +13,7 @@ systematic_quartet.causal_quartet <- function(obj){
   print(paste("ate", ate, sep=" "))
   
   if(space == "observables"){
+    
     #a - linear interaction
     y_a_treat <- y + ate + ate*(x - mean(x))/sd(x)
     
@@ -24,21 +25,40 @@ systematic_quartet.causal_quartet <- function(obj){
     y_c_treat <- invlogit(x-mean(x))
     y_c_treat <- y + y_c_treat*ate/mean(y_c_treat)
     
+    #if provided yrange violated then its not compatible with the ate
+    if( yrange_given && exceeds_bounds(c(y_a_treat,y_b_treat,y_c_treat),yrange)) {
+      warning("Input yrange is not compatible with ATE: Overriding provided yrange.")
+      yrange_given <- FALSE
+    }
+    
     #d
     m <- get_m(ate, x)
-    y_d_treat <- 1/(1 + exp(0.4*(x-mean(x))^2))
-    #print(paste("length y_d", length(y_d), sep=" "))
+    y_d_treat <- 1/(1 + exp(m*(x-mean(x))^2))
     y_d_treat <- y_d_treat*ate/mean(y_d_treat)
     y_d_treat <- y + y_d_treat
-    #print(paste("length y_d", length(y_d), sep=" "))
-    print(paste("y_d_treat", y_d_treat, sep=" "))
     
+    if(yrange_given){ #check if they gave a yrange that is compatible with ATE
+      if(exceeds_bounds(y_d_treat,yrange) ){
+        if(ate > 0){ #if its a positive effect, don't exceed yrange max
+          while( max( y + (1/(1 + exp(m*(x-mean(x))^2)))*ate/mean(1/(1 + exp(m*(x-mean(x))^2)) ) ) > yrange[2]){
+            m <- m*0.5
+          }
+        }else{ #negative ATE - don't exceed yrange min
+          while( min( y + (1/(1 + exp(m*(x-mean(x))^2)))*ate/mean(1/(1 + exp(m*(x-mean(x))^2)) ) ) < yrange[1]){
+            m <- m*0.5
+          } #end while
+        } #end else
+        y_d_treat <- 1/(1 + exp(m*(x-mean(x))^2))
+        y_d_treat <- y_d_treat*ate/mean(y_d_treat)
+        y_d_treat <- y + y_d_treat
+      } #end else if
+    }else{
+      mins <- c(min(y_a_treat),min(y_b_treat),min(y_c_treat),min(y_d_treat),min(y))
+      maxs <- c(max(y_a_treat),max(y_b_treat),max(y_c_treat), max(y_d_treat),max(y))
+      yrange <- c(min(mins), max(maxs))
+    }
+   
     df <- data.frame(y_a_treat, y_b_treat, y_c_treat, y_d_treat)
-    
-    mins <- c(min(y_a_treat),min(y_b_treat),min(y_c_treat),min(y_d_treat),min(y))
-    maxs <- c(max(y_a_treat),max(y_b_treat),max(y_c_treat), max(y_d_treat),max(y))
-    yrange <- c(min(mins), max(maxs))
-  
     
   }else{
     #a - linear interaction
@@ -52,39 +72,28 @@ systematic_quartet.causal_quartet <- function(obj){
     y_c <- invlogit(x-mean(x))
     y_c <- y_c*ate/mean(y_c)
     
-    if( yrange_given && ( ( min(c(y_a,y_b,y_c)) < yrange[1] ) || ( max(c(y_a,y_b,y_c))  > yrange[2] ) ) ) {
+    if( yrange_given && exceeds_bounds(c(y_a,y_b,y_c), yrange) ){
       warning("Input yrange is not compatible with ATE: Overriding provided yrange.")
       yrange_given <- FALSE
-      mins <- c(min(y_a),min(y_b),min(y_c))
-      maxs <- c(max(y_a),max(y_b),max(y_c))
-      yrange <- c(min(mins), max(maxs))
     }
-    #d - intermediate zone
-    #previously: y_d <- 1/(1 + exp(0.4*(x-9.2)^2))
     
+    #d - intermediate zone
     m <- get_m(ate,x)
     y_d <- 1/(1 + exp(m*(x-mean(x))^2))
     y_d <- y_d*ate/mean(y_d)
     
-    #print(paste("length y_d", length(y_d), sep=" "))
-    print(paste("y_d", y_d, sep=" "))
 
     if(yrange_given){ 
-      if(max(y_d) > yrange[2] || min(y_d) < yrange[1] ){
-        print("y_d out of range")
-        if(ate > 0){ #if its a positive effect, care about not exceeding yrange max
+      if(exceeds_bounds(y_d,yrange)) {
+        if(ate > 0){ #if its a positive effect, don't exceed yrange max
           while( max( (1/(1 + exp(m*(x-mean(x))^2)))*ate/mean(1/(1 + exp(m*(x-mean(x))^2)) ) ) > yrange[2]){
-            print(paste("finding new m, previous:", m, sep=" "))
             m <- m*0.5
-            print(paste("new m:", m, sep=" "))
             y_d <- 1/(1 + exp(m*(x-mean(x))^2))
             y_d <- y_d*ate/mean(y_d)
           }
-        }else{ #negative ATE - care about not exceeding yrange min
+        }else{ #negative ATE - don't exceed yrange min
           while( min( (1/(1 + exp(m*(x-mean(x))^2)))*ate/mean(1/(1 + exp(m*(x-mean(x))^2)) ) ) < yrange[1]){
-            print(paste("finding new m, previous:", m, sep=" "))
             m <- m*0.5
-            print(paste("new m:", m, sep=" "))
             y_d <- 1/(1 + exp(m*(x-mean(x))^2))
             y_d <- y_d*ate/mean(y_d)
           } #end while
@@ -95,14 +104,10 @@ systematic_quartet.causal_quartet <- function(obj){
       maxs <- c(max(y_a),max(y_b),max(y_c), max(y_d))
       yrange <- c(min(mins), max(maxs))
     } #end yrange given
+    
     df <- data.frame(y_a, y_b, y_c, y_d)
-    #test if y_d acheives the ate
-    y_d_realized <- sum(y_d)/length(y_d)
-    print(paste("y_d realized", y_d_realized, sep=" "))
   } #end else - latent vs observables 
     
-  
-  
   d <- list(yrange, df[,1], df[,2], df[,3], df[,4])
   names(d) <- c("yr", "y_a", "y_b", "y_c", "y_d")
   return(d)
